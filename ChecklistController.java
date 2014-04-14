1a9366c37f4e0088fadf0c2e38beb58916e96b4b
Vincent Tian <vincenttian16@gmail.com>
2014-04-14 15:20:40 -0700

    initial checklist vf and controller

---
+++ ChecklistController.java
@@ -0,0 +1,13 @@
+global with sharing class ChecklistController {
+
+	public static String getCheckListItems() {
+	   List<Checklist_Item__c> checklists = getAllChecklists(); // pass in ID
+	   return JSON.serialize(checklists);
+	} 
+
+	private static List<Checklist_Item__c> getAllChecklists(){
+        return [SELECT Order__c, Question__c, Required__c, Type__c 
+        		FROM Checklist_Item__c];
+    }
+
+}
