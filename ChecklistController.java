f708bb84a7ae97e24f8cd101b10a56d11703a1ea
Vincent Tian <vincenttian16@gmail.com>
2014-04-18 14:20:09 -0700

    working backend for checklist submission

--- ChecklistController.java
+++ ChecklistController.java
@@ -10,4 +10,20 @@ global with sharing class ChecklistController {
         		FROM Checklist_Item__c WHERE Checklist__c=:ApexPages.currentPage().getParameters().get('checklist_id')];
     }
 
+    // Creates Checklist Response Item Objects
+    @RemoteAction
+    global static String[] save_responses(Id checklist, Id[] checklist_items, String[] answers) {
+        Checklist_Reponse__c new_response = new Checklist_Reponse__c();
+        new_response.Checklist__c = checklist;
+        insert(new_response);
+
+        for (Integer i=0; i<checklist_items.size(); i++) {
+        	Checklist_Item_Response__c new_item_response = new Checklist_Item_Response__c();
+	        new_item_response.Checklist_Item__c = checklist_items[i];
+	        new_item_response.Checklist_Reponse__c = new_response.Id;
+	        new_item_response.Answer__c = answers[i];
+	        insert new_item_response;
+        }
+        return new String[]{}; // can include succes message?
+    }
 }
