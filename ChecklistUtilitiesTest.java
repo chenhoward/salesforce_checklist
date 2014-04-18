8f69c2cd08b3c52b9bef8e31545980b5d682a092
Howard Chen <howardxchen@gmail.com>
2014-04-18 16:05:15 -0700

    Add update tests for checklist items

--- ChecklistUtilitiesTest.java
+++ ChecklistUtilitiesTest.java
@@ -4,7 +4,7 @@ public class ChecklistUtilitiesTest {
 
     /** Tests the creation of a Checklist. */
     static testMethod void testCreateChecklist() {
-    	Checklist__c test;
+        Checklist__c test;
         test = ChecklistUtilities.createChecklist(null, null);
         test = ChecklistUtilities.findChecklist(test.ID);
         System.assertNotEquals(test, null);
@@ -24,16 +24,22 @@ public class ChecklistUtilitiesTest {
 
 /** Tests the creation of a Checklist item. */
     static testMethod void testCreateChecklistItem() {
-    	Checklist__c checklist= ChecklistUtilities.createChecklist('nom', 'desc');
-    	Checklist_Item__c item1 = new Checklist_Item__c(Order__c = 5, Question__c = 'Age?',
-    		Required__c = true);
-    	Checklist_Item__c item2 = new Checklist_Item__c(Order__c = 0, Question__c = 'Size?',
-    		Required__c = true);
+        Checklist__c checklist= ChecklistUtilities.createChecklist('nom', 'desc');
+        Checklist_Item__c item1 = new Checklist_Item__c(Order__c = 5, Question__c = 'Age?',
+            Required__c = true);
+        Checklist_Item__c item2 = new Checklist_Item__c(Order__c = 0, Question__c = 'Size?',
+            Required__c = true);
         Checklist_Item__c[] itemList = new Checklist_Item__c[]{item1, item2};
         System.assertEquals(itemList.get(0).Question__c, 'Age?');
-    	ChecklistUtilities.addChecklistItems(checklist, itemList);
+        ChecklistUtilities.addChecklistItems(checklist, itemList);
         Checklist_Item__c[] itemListCheck = ChecklistUtilities.findChecklistItems(checklist);
         System.assertEquals(itemListCheck.size(), 2);
         System.assertEquals(itemListCheck.get(0).Question__c, 'Size?');
+        item1.Order__c = 1;
+        item2.Order__c = 2;
+        ChecklistUtilities.updateChecklistItems(itemList);
+        itemListCheck = ChecklistUtilities.findChecklistItems(checklist);
+        System.assertEquals(itemListCheck.size(), 2);
+        System.assertEquals(itemListCheck.get(0).Question__c, 'Age?');
     }
 }
