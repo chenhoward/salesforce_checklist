60b4c1edfbfa4bc484a6a667360ffe4f75c9fc88
Howard Chen <howardxchen@gmail.com>
2014-04-18 14:56:35 -0700

    VF page can create surveys

--- ChecklistController.java
+++ ChecklistController.java
@@ -1,13 +1,10 @@
-global with sharing class ChecklistController {
+global with sharing class ChecklistAdminController {
 
-	public static String getCheckListItems() {
-	   List<Checklist_Item__c> checklists = getAllChecklists(); // pass in ID
-	   return JSON.serialize(checklists);
-	} 
 
-	private static List<Checklist_Item__c> getAllChecklists(){
-        return [SELECT Order__c, Question__c, Required__c, Type__c, Checklist__c 
-        		FROM Checklist_Item__c WHERE Checklist__c=:ApexPages.currentPage().getParameters().get('checklist_id')];
+    /** Remote call used by admin pages to create a checklist. */
+    @RemoteAction
+    global static Checklist__c createChecklist(String listName, String description) {
+        Checklist__c checklist = ChecklistUtilities.createChecklist(listName, description);
+        return checklist;
     }
-
 }
