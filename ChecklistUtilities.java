f04272665ce4f1ecdd8140ceb8462d9c9c5027f3
Howard Chen <howardxchen@gmail.com>
2014-04-18 11:37:12 -0700

    CHecklistUtilities now works

--- ChecklistUtilities.java
+++ ChecklistUtilities.java
@@ -1,22 +1,23 @@
 /** A utility class that helps create Checklists and Checklist related items. */
 global class ChecklistUtilities {
 
-    /** Returns a new Checklist with the name LISTNAME, and the description DESC
+    /** Returns a new Checklist with the name LISTNAME, and the DESCRIPTION
       * while inserting it into the database. */
-    global static Checklist__c createChecklist(String listName, String desc) {
-        Checklist__c checklist = new Checklist__c(Name = listName, Description__c = desc);
+    global static Checklist__c createChecklist(String listName, String description) {
+        Checklist__c checklist = new Checklist__c(Name = listName, Description__c = description);
         insert checklist;
         return checklist;
     }
 
     /** Removes a Checklist with the ID LISTID. */
     global static void removeChecklist(ID listID) {
-        remove findChecklist(listID);
+        Checklist__c checklist = findChecklist(listID);
+        delete checklist;
     }
 
     /** Queries the database for a Checklist with the ID LISTID. */
     global static Checklist__c findChecklist(ID listID) {
-        Checklist__c[] checklist = [SELECT Name, Id, Description__c FROM Checklist__c WHERE Id := listID];
+        Checklist__c[] checklist = [SELECT ID, Name, Description__c FROM Checklist__c WHERE ID=:listID];
         return checklist[0];
     }
 }
