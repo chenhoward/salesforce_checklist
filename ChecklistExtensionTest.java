24c83042b487813da6fde2eaec781c830a061f9f
Howard Chen <howardxchen@gmail.com>
2014-06-10 16:52:53 -0700

    Add items test

--- ChecklistExtensionTest.java
+++ ChecklistExtensionTest.java
@@ -2,14 +2,19 @@
 @isTest
 public class ChecklistExtensionTest{
 
-    static void listMaker(String s) {
+    static Id listMaker(String s) {
         Checklist__c checklist = ChecklistUtilities.createChecklist('c', null);
         Checklist_Response__c response = new Checklist_Response__c();
         response.Checklist__c = checklist.Id;
         response.Status__c = s;
         response.Responder__c = UserInfo.getUserId();
+        Checklist_Item__c item = new Checklist_Item__c();
+        item.Checklist__c = checklist.Id;
+        insert item;
         insert response;
+        return checklist.Id;
     }
+
     static testmethod void testPending() {
 	    listMaker('Pending');
         System.assertEquals(ChecklistExtension.pendingChecklists().size(), 1);
@@ -18,5 +23,10 @@ public class ChecklistExtensionTest{
     static testmethod void testCompleted() {
         listMaker('Complete');
         System.assertEquals(ChecklistExtension.completedChecklists().size(), 1);
+    }
+    
+    static testmethod void testGetChecklistItems() {
+        Id d= listMaker('Compete');
+        System.assertEquals(ChecklistExtension.getAllChecklistItems(d).size(), 1);
     }
 }
