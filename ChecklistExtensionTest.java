101136e3c3767a042e1730e03c96f66ce7ecc200
Howard Chen <howardxchen@gmail.com>
2014-06-10 16:02:37 -0700

    Add basic testing

--- ChecklistExtensionTest.java
+++ ChecklistExtensionTest.java
@@ -1,28 +1,16 @@
 /** The test for ChecklistExtension. */
 @isTest
-public class ChecklistExtensionTest {
+public class ChecklistExtensionTest{
 
-    /** Tests the completion of a Checklist Response. */
-    static testMethod void test_finish_checklist_items() {
-        Checklist__c testC = new Checklist__c(Name='Test Checklist');
-        Checklist_Response__c newResp = new Checklist_Response__c(Status='Pending', Checklist=testC);
-        Integer numPending = [SELECT count() FROM Checklist_Response__c WHERE Status__c = 'Pending'];
-        Integer numCompleted = [SELECT count() FROM Checklist_Response__c WHERE Status__c = 'Complete'];
-        Checklist__c test = ChecklistExtension.finish_checklist_items(newResp);
-        Integer numPendingAfter = [SELECT count() FROM Checklist_Response__c WHERE Status__c = 'Pending'];
-        Integer numCompletedAfter = [SELECT count() FROM Checklist_Response__c WHERE Status__c = 'Complete'];
-        System.assertEquals(numPending-1, numPendingAfter);
-        System.assertEquals(numCompleted+1, numCompletedAfter);
+    static testmethod void testPending() {
+	    Checklist__c checklist = ChecklistUtilities.createChecklist('c', null);
+        Checklist_Response__c response = new Checklist_Response__c();
+        response.Checklist__c = checklist.Id;
+        response.Status__c = 'Pending';
+        response.Responder__c = UserInfo.getUserId();
+        insert response;
+        System.assertEquals(ChecklistExtension.pendingChecklists().size(), 1);
+
     }
 
-    /** Tests the submission of Checklist responses. */
-    static testMethod void test_submit_responses() {
-        Checklist__c testC = new Checklist__c(Name='Test Checklist');
-        
-    }
-
-    /** Tests the saving of Checklist responses. */
-    static testMethod void test_save_responses() {
-        System.assertEquals(itemListCheck.get(0).Question__c, 'Age?');
-    }
 }
